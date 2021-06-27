2f8e7a5adbdcaa2a053b48a78e5d47fb681780b5
Ha Hai <haihd@users.noreply.github.com>
2021-06-27 17:37:31 +0900

    Initial Commit

---
+++ Cookie Alarm/Cookie_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_AlarmApp.swift
+//  Cookie Alarm
+//
+//  Created by Hai Ha on 2021/06/27.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cookie AlarmTests/Cookie_AlarmTests.swift
@@ -0,0 +1,33 @@
+//
+//  Cookie_AlarmTests.swift
+//  Cookie AlarmTests
+//
+//  Created by Hai Ha on 2021/06/27.
+//
+
+import XCTest
+@testable import Cookie_Alarm
+
+class Cookie_AlarmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
